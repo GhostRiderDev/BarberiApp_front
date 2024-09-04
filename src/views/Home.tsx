2b462340720a487001ec1a6f2f77367f8ef3cc0f
Sebastian Guerra <149134186+SebastianDevps@users.noreply.github.com>
2024-09-03 21:20:44 -0500

    Update New

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,13 @@
+import CardBarber from "../components/CardBarber";
+
 const Home = () => {
   return (
-    <>
-      <div className="bg-home"></div>
-    </>
+    <div className="w-full xl:w-full h-[100vh]">
+      <div className="bg-home bg-center bg-cover w-full h-[700px]">
+        <h1 className="p-4 text-white text-4xl">Recomendaciones</h1>
+        <CardBarber />
+      </div>
+    </div>
   );
 };
 
