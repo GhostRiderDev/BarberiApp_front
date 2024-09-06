d370371d56a19db54e24cc8ecd6e033f6951a075
Sebastian Guerra <149134186+SebastianDevps@users.noreply.github.com>
2024-09-05 20:36:48 -0500

    New Update

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,39 +1,23 @@
 import Services from "../components/Services";
-import Services from "../components/Services";
 import CardBarber from "../components/CardBarber";
 import NavbarLanding from "../components/NavbarLanding";
 import SearchBarber from "../components/search-barber/SearchBarber";
 
 const Home: React.FC = () => {
-const Home: React.FC = () => {
   return (
-    <>
-      <div className="w-full xl:w-full h-[100vh]">
-        {/* <NavbarLanding /> */}
-        <div className="bg-home bg-center gap-20 flex flex-col justify-center items-center bg-cover w-full h-[700px]">
-          <SearchBarber />
-          <div className="w-full h-[350px] justify-center lg:justify-start">
-            <h1 className="lg:mx-10 mx-3 text-white text-4xl  font-bold">Recomendaciones</h1>
-            <CardBarber />
-          </div>
 
-        </div>
-    <>
-      <div className="w-full xl:w-full h-[100vh]">
-        {/* <NavbarLanding /> */}
-        <div className="bg-home bg-center gap-20 flex flex-col justify-center items-center bg-cover w-full h-[700px]">
-          <SearchBarber />
-          <div className="w-full h-[350px] justify-center lg:justify-start">
-            <h1 className="lg:mx-10 mx-3 text-white text-4xl  font-bold">Recomendaciones</h1>
-            <CardBarber />
-          </div>
-
+    <div className="w-full xl:w-full h-[100vh]">
+      {/* <NavbarLanding /> */}
+      <div className="bg-home bg-center gap-20 flex flex-col justify-center items-center bg-cover w-full h-[700px]">
+        <SearchBarber />
+        <div className="w-full h-[350px] justify-center lg:justify-start">
+          <h1 className="lg:mx-10 mx-3 text-white text-4xl  font-bold">Recomendaciones</h1>
+          <CardBarber />
         </div>
       </div>
       <Services />
-    </>
-      <Services />
-    </>
+    </div>
+
   );
 };
 
