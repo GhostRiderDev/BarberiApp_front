a96f06a1d01053a0ce5793ea0c41121b62b893c4
LuisRGN <luisrafael1598@gmail.com>
2024-09-05 20:32:06 -0500

    Se cambia la imagen por un gradiante y se acomodan el home para poder usarlo, se ajusta el buscador el navbar y el footer

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,23 +1,22 @@
 import Services from "../components/Services";
 import CardBarber from "../components/CardBarber";
-// import NavbarLanding from "../components/NavbarLanding";
+import NavbarLanding from "../components/NavbarLanding";
 import SearchBarber from "../components/search-barber/SearchBarber";
 
 const Home: React.FC = () => {
   return (
     <>
-      <div className="w-full xl:w-full h-[100vh]">
-        {/* <NavbarLanding /> */}
-        <div className="bg-home bg-center gap-20 flex flex-col justify-center items-center bg-cover w-full h-[700px]">
+      <div className="w-full xl:w-full ">
+        <NavbarLanding />
+        <div className="bg-home  gap-20 flex flex-col justify-center items-center w-full h-[50rem]">
           <SearchBarber />
-          <div className="w-full h-[350px] justify-center lg:justify-start">
-            <h1 className="lg:mx-10 mx-3 text-white text-4xl  font-bold">Recomendaciones</h1>
+          <div className="w-full h-[350px] justify-center lg:justify-start md:max-w-[1400px] md:mx-auto">
+            <h1 className="mx-3 text-4xl font-bold text-center text-white md:text-start lg:mx-10">Recomendaciones</h1>
             <CardBarber />
           </div>
-
         </div>
+        <Services />
       </div>
-      <Services />
     </>
   );
 };
