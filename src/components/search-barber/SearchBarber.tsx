ccc32809751ac06289c5785b4c3832e1302c7380
cos4h <olvadis2004@gmail.com>
2024-09-09 22:49:44 -0500

    Terminado componente de busquedad

--- src/components/search-barber/SearchBarber.tsx
+++ src/components/search-barber/SearchBarber.tsx
@@ -1,5 +1,4 @@
 import AutoCompleteAdress from "@/components/AutoCompleteAdress";
-
 const SearchBarber = () => {
   return (
     <div className="w-[360px] mt-16 max-[350px]:w-[95%] lg:w-[500px] gap-4 flex flex-col justify-center text-center items-center bg-[#d6d6d638] p-4 rounded-[20px] ">
---
+++ src/services/barberiaService.ts
@@ -0,0 +1,20 @@
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const findBarberiasByCityAndProvince = async (
+  city: string,
+  province: string
+) => {
+  try {
+    const response = await fetch(
+      `${apiUrl}/barberia/ubication?ciudad=${city}&departamento=${province}`
+    );
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
