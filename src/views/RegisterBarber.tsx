87f4cc53dcab18794c271c099c453183221d5fd2
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-09-20 00:41:10 -0500

    creacion de formulario de registro barberos

---
+++ src/views/RegisterBarber.tsx
@@ -0,0 +1,15 @@
+import { useRef } from "react";
+import NavbarBarber from "@/components/NavbarBarber";
+import FromRegisterBarber from "@/components/FromRegisterBarber";
+const RegisterBarber = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  return (
+    <div className="h-full w-full bg-gray-50">
+      <NavbarBarber formRef={formRef} />
+      <FromRegisterBarber formRef={formRef} />
+    </div>
+  );
+};
+
+export default RegisterBarber;
